552601749fc4c00bcdae3d459eaacae014c51c46
ecoffet <simon.ecoffet@gmail.com>
2021-04-12 16:01:58 +0200

    remove comment

--- src/App.js
+++ src/App.js
@@ -8,15 +8,9 @@ function App() {
     // Send the request
     axios
       .get('https://simpsons-quotes-api.herokuapp.com/quotes?num=1')
-      // Extract the DATA from the received response
       .then((response) => response.data)
-      
-      // Use this data to update the state
       .then((data) => {
-        console.log(data)
         setSimpson([...data]);
-    
-        
       });
   };
   const [Simpson, setSimpson] = React.useState("");
